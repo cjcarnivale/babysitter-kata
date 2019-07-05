62d1787769e3325bcb84483ad22ff7280e020702
Christopher <christopher.carnivale@gmail.com>
2019-07-05 18:12:17 -0400

    agruments test passing

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'; 
+
+module.exports = function hourCalculator(startTime, endTime, bedTime) {
+  if (!bedTime) {
+    throw new Error('Must enter start time, end time, and bed time'); 
+  }
+}; 
