d74c208e06aa998b73171e183fb6cc50a19c11f7
Christopher <christopher.carnivale@gmail.com>
2019-07-05 18:16:46 -0400

    test for start time before 5:00 p.m.

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,6 +7,13 @@
 - gets paid for full hours (no fractional hours)
 */
 
+/*
+Assumptions: 
+1. a.m. and p.m. are unnecceary for the arguments because there is no overlap. Therefore, anything before 5:00
+  for the start time and anything after 4:00 for the end time should throw an error.
+
+*/
+
 'use strict'; 
 
 const hourCalculator = require('../index'); 
@@ -15,5 +22,9 @@ const { expect } = require('chai');
 describe('Hour Calculator', () => {
   it('should throw an error if all arguments aren\'t present', () => {
     expect(() => hourCalculator('5:00', '9:00')).to.throw('Must enter start time, end time, and bed time');   
+  });
+
+  it('should throw an error if start time is earlier than 5:00 p.m.', () => {
+    expect(() => hourCalculator('4:00', '10:00', '9:00')).to.throw('Start time must be 5:00 p.m. or later');
   });
 });
